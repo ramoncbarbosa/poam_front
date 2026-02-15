6c8da8e955695c99baec2faf482cedf759b61cb4
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2026-02-15 15:17:02 -0300

    a

--- database/users.js
+++ database/users.js
@@ -1,50 +1,50 @@
 // scripts/users.js
 export const teamData = [
-  { 
-    nome: 'Dra. Maria Dolores', 
-    titulo: 'Doutora em Ciência Política', 
+  {
+    nome: 'Dra. Maria Dolores',
+    titulo: 'Doutora em Ciência Política',
     cargo: 'Coordenadora Geral',
     areaPesquisa: 'Especialista em gestão pública amazônica.',
     foto: 'assets/users/maria_dolores.jpg'
   },
-  { 
-    nome: 'Dra. Eugênia Rosa Cabral', 
-    titulo: 'Doutora em Políticas de Sustentabilidade', 
+  {
+    nome: 'Dra. Eugênia Rosa Cabral',
+    titulo: 'Doutora em Políticas de Sustentabilidade',
     cargo: 'Coordenadora Adjunta',
     areaPesquisa: 'Doutora e pesquisadora em Políticas de Sustentabilidade.',
     foto: 'assets/users/eugenia_rosa.jpg'
   },
-  { 
-    nome: 'Dâina Naíny Cunha', 
-    titulo: 'Mestra em Ciências Sociais', 
+  {
+    nome: 'Dâina Naíny Cunha',
+    titulo: 'Mestra em Ciências Sociais',
     cargo: 'Pesquisadora Principal',
     areaPesquisa: 'Ciências Sociais e análise de políticas públicas.',
     foto: 'assets/users/daina_nainy.jpg'
   },
-  { 
-    nome: 'Ricardo Bezerra', 
-    titulo: 'Mestre em Ecologia', 
+  {
+    nome: 'Ricardo Bezerra',
+    titulo: 'Mestre em Ecologia',
     cargo: 'Pesquisador',
     areaPesquisa: 'Ecologia e legislação ambiental amazônica.',
-    foto: 'assets/users/ricardo_bezerra.jpg'
+    foto: 'https://ramoncbarbosa.github.io/poam_front/assets/users/ricardo_bezerra.jpg'
   },
-  { 
-    nome: 'Aline Silva', 
-    titulo: 'Especialista em Direito Ambiental', 
+  {
+    nome: 'Aline Silva',
+    titulo: 'Especialista em Direito Ambiental',
     cargo: 'Pesquisadora',
     areaPesquisa: 'Direito Ambiental e governança de terras.',
     foto: 'assets/users/aline_silva.jpg'
   },
-  { 
-    nome: 'João Mendes', 
-    titulo: 'Mestre em Geoprocessamento', 
+  {
+    nome: 'João Mendes',
+    titulo: 'Mestre em Geoprocessamento',
     cargo: 'Pesquisador',
     areaPesquisa: 'Geoprocessamento e mineração de dados políticos.',
     foto: 'assets/users/joao_mendes.jpg'
   },
-  { 
-    nome: 'Lucas Costa', 
-    titulo: 'Especialista em Análise de Dados', 
+  {
+    nome: 'Lucas Costa',
+    titulo: 'Especialista em Análise de Dados',
     cargo: 'Pesquisador',
     areaPesquisa: 'Análise de dados territoriais.',
     foto: 'assets/users/lucas_costa.jpg'
